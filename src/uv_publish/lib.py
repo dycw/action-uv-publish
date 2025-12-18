4b9db4fbfbdb969db4f1a13a818f97b3a9a945a2
Derek Wan <d.wan@icloud.com>
2025-12-18 15:33:53 +0900

    2025-12-18 15:33:53 (Thu)  > DW-Mac  > derekwan

--- src/uv_publish/lib.py
+++ src/uv_publish/lib.py
@@ -8,13 +8,24 @@ from uv_publish.logging import LOGGER
 from uv_publish.settings import SETTINGS
 
 
-def uv_publish(*, trusted_publishing: bool = SETTINGS.trusted_publishing) -> None:
+def uv_publish(
+    *,
+    username: str | None = SETTINGS.username,
+    password: str | None = SETTINGS.password,
+    publish_url: str | None = SETTINGS.publish_url,
+    trusted_publishing: bool = SETTINGS.trusted_publishing,
+    native_tls: bool = SETTINGS.native_tls,
+) -> None:
     with TemporaryDirectory() as temp:
         _log_run("uv", "build", "--out-dir", str(temp), "--wheel", "--clear")
         _log_run(
             "uv",
             "publish",
+            *([] if username is None else ["--username", username]),
+            *([] if password is None else ["--password", password]),
+            *([] if publish_url is None else ["--publish-url", publish_url]),
             *(["--trusted-publishing", "always"] if trusted_publishing else []),
+            *(["--native-tls"] if native_tls else []),
             f"{temp}/*",
         )
 
